c8de0243f018551b718181e955392f84b9ac4762
baoning.yang <baoning.yang@transsion.com>
2019-11-17 16:49:01 +0800

    加入显示天气信息的功能

---
+++ app/src/main/java/com/coolWeather/android/gson/AQI.java
@@ -0,0 +1,11 @@
+package com.coolWeather.android.gson;
+
+public class AQI {
+
+    public AQIcity city;
+
+    public class AQIcity{
+        public String aqi;
+        public String pm25;
+    }
+}
